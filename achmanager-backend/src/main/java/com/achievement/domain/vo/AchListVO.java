80130f420a6ec421945c1654de5259989c474266
TiKi <3475035979@qq.com>
2026-01-26 13:29:20 +0800

    本次修复了科研看台中，最新入库成果模块 不显示作者的问题

--- achmanager-backend/src/main/java/com/achievement/domain/vo/AchListVO.java
+++ achmanager-backend/src/main/java/com/achievement/domain/vo/AchListVO.java
@@ -11,7 +11,7 @@ public class AchListVO {
     private String typeName;       // 成果物类型
     private String auditStatus;    // 审核状态
     private String creatorName;    // 创建者用户名称
-    private String authorName;     // 成果物作者（若与创建者不同，按你的表字段映射）
+    private String author;     // 成果物作者（若与创建者不同，按你的表字段映射）
     private String projectName;    // 所属项目名称
     private String summary;        // 成果物概要
     private LocalDateTime createdAt; //创建时间
